9ccdb0f9e754f17e83d9c1f349ec9758d8a86255
Sogurung16 <Sogurung16@gmail.com>
2021-08-16 17:27:15 +0100

    SelectionSort Implemented & all passing tests for SelectionSort

--- src/main/java/com/sparta/sonam/sortmanager/sorters/SelectionSort.java
+++ src/main/java/com/sparta/sonam/sortmanager/sorters/SelectionSort.java
@@ -9,7 +9,26 @@ public class SelectionSort implements Sorter{
 
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        return new int[0];
+        return getSelectionSortedArray(arrayToSort);
+    }
+
+    private int[] getSelectionSortedArray(int[] arrayToSort){
+        selectionSort(arrayToSort);
+        int[] sortedArray = arrayToSort;
+        return sortedArray;
+    }
+
+    private void selectionSort(int[] arrayToSort){
+        int temp;
+        for(int i = 0; i < arrayToSort.length; i++){
+            for(int j = i+1; j < arrayToSort.length; j++){
+                if(arrayToSort[j]<arrayToSort[i]){
+                    temp = arrayToSort[i];
+                    arrayToSort[i] = arrayToSort[j];
+                    arrayToSort[j] = temp;
+                }
+            }
+        }
     }
 
     @Override
--- src/test/java/com/sparta/sonam/sortmanager/sorters/SelectionSortTest.java
+++ src/test/java/com/sparta/sonam/sortmanager/sorters/SelectionSortTest.java
@@ -5,18 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
-//TODO: Performance testing all sorts with same arrays
-
-@DisplayName("Should MergeSort the provided array in ascending order")
-class MergeSortTest implements SortersTestInterface{
+class SelectionSortTest implements SortersTestInterface{
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted array")
+    @DisplayName("Returns a SelectionSorted array")
     public void sortArrayTest() {
         int[] unsortedArray = new int[]{24,5,2,19,67,78,59,99};
 
         int[] expectedSortedArray = new int[]{2,5,19,24,59,67,78,99};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
@@ -26,12 +23,12 @@ class MergeSortTest implements SortersTestInterface{
 
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted array. Test for value zero in array")
+    @DisplayName("Returns a SelectionSorted array. Test for value zero in array")
     public void sortArrayWithZeroTest(){
         int[] unsortedArray = new int[]{24,5,2,0,67,78,59,99};
 
         int[] expectedSortedArray = new int[]{0,2,5,24,59,67,78,99};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
@@ -41,12 +38,12 @@ class MergeSortTest implements SortersTestInterface{
 
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted array. Test for negative values in array")
+    @DisplayName("Returns a SelectionSorted array. Test for negative values in array")
     public void sortArrayWithNegativesTest(){
         int[] unsortedArray = new int[]{24,5,2,-67,78,-59,99,-40};
 
         int[] expectedSortedArray = new int[]{-67,-59,-40,2,5,24,78,99};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
@@ -56,12 +53,12 @@ class MergeSortTest implements SortersTestInterface{
 
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted array. Test for repeated values in array")
+    @DisplayName("Returns a SelectionSorted array. Test for repeated values in array")
     public void sortArrayWithRepeatsTest(){
         int[] unsortedArray = new int[]{19,5,2,67,19,67,19,99};
 
         int[] expectedSortedArray = new int[]{2,5,19,19,19,67,67,99};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
@@ -71,12 +68,12 @@ class MergeSortTest implements SortersTestInterface{
 
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted even size array of length 8")
+    @DisplayName("Returns a SelectionSorted even size array of length 8")
     public void sortArrayOfEvenSize(){
         int[] unsortedArray = new int[]{24,5,2,19,67,78,59,99};
 
         int[] expectedSortedArray = new int[]{2,5,19,24,59,67,78,99};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
@@ -86,12 +83,12 @@ class MergeSortTest implements SortersTestInterface{
 
     @Override
     @Test
-    @DisplayName("Returns a MergeSorted odd size array of length 7")
+    @DisplayName("Returns a SelectionSorted odd size array of length 7")
     public void sortArrayOfOddSize(){
         int[] unsortedArray = new int[]{24,5,2,19,67,78,59};
 
         int[] expectedSortedArray = new int[]{2,5,19,24,59,67,78};
-        int[] actualSortedArray = SortFactory.getSortingAlgorithm(3).sortArray(unsortedArray);
+        int[] actualSortedArray = SortFactory.getSortingAlgorithm(5).sortArray(unsortedArray);
 
         String expectedArray = Arrays.toString(expectedSortedArray);
         String actualArray = Arrays.toString(actualSortedArray);
